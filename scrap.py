84d498ead68c642eacbc0f787ad89cf616aab827
jisbruzzi <jose.sbruzzi@ayi-asociados.com>
2018-09-21 09:33:08 -0300

    Agrego correlativas

--- scrap.py
+++ scrap.py
@@ -35,7 +35,8 @@ with Path("materias_deptos.sql").open("w") as sql:
         
     materias=[] 
     values=[]
-    Correlativas=namedtuple("Correlativas",["cm_para","cd_para","cm_necesario","cd_necesario"])
+    Correlatividad=namedtuple("Correlatividad",["cm_para","cd_para","cm_necesario","cd_necesario"])
+    correlativas=[]
     for filename in path_carreras.glob("*.txt"):
         carrera=str(filename).split("-")[0].split("/")[1]
         for linea in filename.open().read().split("\n"):
@@ -46,6 +47,7 @@ with Path("materias_deptos.sql").open("w") as sql:
 
             campos=linea.split("	")
             nombre_materia=campos[1]
+
             codigo=campos[0]
             codigo_depto=""
             codigo_materia=""
@@ -55,13 +57,38 @@ with Path("materias_deptos.sql").open("w") as sql:
             else:
                 codigo_depto=codigo.split(" ")[0]
                 codigo_materia=codigo.split(" ")[1]
+
+
             nombre_materia=campos[1]
             creditos_materia=campos[2]
-            correlativas_texto=campos[3]
-            correlativas=re.findall("[0-9][0-9]\.[0-9][0-9]",correlativas_texto)
+
+
+            
+
             correlativas_materia=""
             try:
-                correlativas_materia=campos[3]#lo de las correlativas lo dejamos para más adelante
+                correlativas_texto=campos[3]
+                correlativas_normales=re.findall("[0-9][0-9]\.[0-9][0-9]",correlativas_texto)
+                for correlativa_normal in correlativas_normales:
+                    codigo_depto_correlativa=correlativa_normal.split(".")[0]
+                    codigo_materia_correlativa=correlativa_normal.split(".")[1]
+                    correlativas.append(Correlatividad(
+                        codigo_materia,
+                        codigo_depto,
+                        codigo_materia_correlativa,
+                        codigo_depto_correlativa
+                    ))
+
+                correlativas_raras=re.findall("[A-Z][A-Z][A-Z] [0-9]",correlativas_texto)
+                for correlativa_raras in correlativas_raras:
+                    codigo_depto_correlativa=correlativa_raras.split(" ")[0]
+                    codigo_materia_correlativa=correlativa_raras.split(" ")[1]
+                    correlativas.append(Correlatividad(
+                        codigo_materia,
+                        codigo_depto,
+                        codigo_materia_correlativa,
+                        codigo_depto_correlativa
+                    ))
             except:
                 pass
             
@@ -73,6 +100,17 @@ with Path("materias_deptos.sql").open("w") as sql:
     tuples=(",\n    ").join(["('{}','{}','{}')".format(m.codigo_depto,m.codigo_materia,m.nombre) for m in materias])
     sql.write("insert into subjects(department_code,code,name) values {};\n".format(tuples))
     sql.write("insert into credits(department_code,subject_code,degree,amount) values {};\n".format(",\n    ".join(values)))
+
+
+
+    
+    correlative_tuples_text=",\n    ".join([
+        "('{}','{}','{}','{}')".format(c.cd_para,c.cm_para,c.cd_necesario,c.cm_necesario) 
+        for c in correlativas
+    ])
+    print("correlative tuples:")
+    #print(correlative_tuples_text)
+    sql.write("insert into requires(dept,code,dept_required,code_required) values {};\n".format(correlative_tuples_text))
 
 
 
