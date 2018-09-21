1c0aab332dbcb1085a54ed4c4b5b4379703e1e6a
jisbruzzi <jose.sbru@gmail.com>
2018-09-21 07:56:09 -0300

    Más scrapping

--- scrap.py
+++ scrap.py
@@ -2,6 +2,7 @@ from pathlib import Path
 from collections import namedtuple
 import re
 here=Path(".")
+path_carreras=Path("carreras")
 Materia=namedtuple("Materia",["codigo_depto","codigo_materia","nombre"])
 with Path("materias_deptos.sql").open("w") as sql:
     for filename in here.glob("*.txt"):
@@ -32,4 +33,39 @@ with Path("materias_deptos.sql").open("w") as sql:
         
         tuples=(",\n    ").join(["('{}','{}','{}')".format(m.codigo_depto,m.codigo_materia,m.nombre) for m in materias])
         sql.write("insert into subjects(department_code,code,name) values {};\n".format(tuples))
+    
+    for filename in path_carreras.glob("*.txt"):
+        carrera=str(filename).split("-")[0].split("/")[1]
+        values=[]
+        for linea in filename.open().read().split("\n"):
+            if len(linea)==0:
+                continue;
+            print(filename)
+            print(linea)
+
+            campos=linea.split("	")
+            codigo=campos[0]
+            codigo_depto=""
+            codigo_materia=""
+            if len(codigo.split("."))==2:
+                codigo_depto=codigo.split(".")[0]
+                codigo_materia=codigo.split(".")[1]
+            else:
+                codigo_depto=codigo.split(" ")[0]
+                codigo_materia=codigo.split(" ")[1]
+            nombre_materia=campos[1]
+            creditos_materia=campos[2]
+            correlativas_materia=""
+            try:
+                correlativas_materia=campos[3]#lo de las correlativas lo dejamos para más adelante
+            except:
+                pass
+            
+            if creditos_materia=="continúa":
+                continue
+
+            values.append("('{}','{}','{}',{})/*{}*/".format(codigo_depto,codigo_materia,carrera,creditos_materia,linea))
+        sql.write("insert into credits(department_code,subject_code,degree,amount) values {};\n".format(",\n    ".join(values)))
+
+
 
